7bbbdc036569f24de857d35e37d3d434d87cce2a
Jack Shendrikov <jackshendrikov@gmail.com>
2021-04-27 13:49:04 +0300

    Fix prediction functions

--- analyzer/views/prediction.py
+++ analyzer/views/prediction.py
@@ -1,50 +1,30 @@
-import os
-
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, render
 
-from analyzer.utils import staff_check
+from analyzer.utils import staff_check, predict, convert_pdf_to_txt
 from analyzer.models import Attachment
-
-
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
-
-
-def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos=set()
-
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
 
 
 @login_required
 @user_passes_test(staff_check)
-def prediction(request, attachment_id: int):
+def prediction(request, attachment_id: int) -> HttpResponse:
+    """ Prediction make sentiment analysis of given pdf attachment and return result specific percent of positivity.
+        Redirect to the prediction page.
+    """
     if request.method == "POST":
-        attachment = Attachment.objects.get(id=attachment_id)
-        pdf = convert_pdf_to_txt(attachment.file.path)
-        print(pdf)
+        attachment = get_object_or_404(Attachment, pk=attachment_id)
 
+        # Permissions
+        if not (attachment.task.task_list.group in request.user.groups.all() or request.user.is_superuser):
+            raise PermissionDenied
 
+        txt = convert_pdf_to_txt(attachment.file.path)
+
+        txt_prediction = predict(txt)
+
+        print("Prediction is :", float(txt_prediction))
+        context = {'prediction': round(float(txt_prediction), 2) * 100}
+
+        return render(request, 'analyzer/predict.html', context)
